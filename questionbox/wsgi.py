9f631b988008604a94d7ca08c86c46555a8cfd22
Middleway13 <middlewayonlinemedia@gmail.com>
2021-09-15 11:13:25 -0400

    update for renaming app

--- questionbox/wsgi.py
+++ questionbox/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Questionbox.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'questionbox.settings')
 
 application = get_wsgi_application()
