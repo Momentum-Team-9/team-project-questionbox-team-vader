5df86af3024202470986c0152accffa317e5df03
Middleway13 <middlewayonlinemedia@gmail.com>
2021-09-17 12:38:36 -0400

    modified answer model question field

--- core/models.py
+++ core/models.py
@@ -4,25 +4,25 @@ from django.contrib.auth.models import AbstractUser
 from datetime import date
 
 class User(AbstractUser):
-    # def create_user(self, email, password):
-    #     """
-    #     Creates and saves a User with the given email, date of
-    #     birth and password.
-    #     """
-    #     if not email:
-    #         raise ValueError('Users must have an email address')
+    def create_user(self, email, password):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
 
-    #     user=self.model(
-    #         email=self.normalize_email(email),
-    #     )
+        user=self.model(
+            email=self.normalize_email(email),
+        )
 
-    #     user.set_password(password)
-    #     user.save(using=self._db)
-    #     return user
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
     
-    # USER_CREATE_PASSWORD_RETYPE=True
-    # USERNAME_FIELD = 'username'
-    # REQUIRED_FIELDS = ['first_name','last_name', 'email']
+    USER_CREATE_PASSWORD_RETYPE=True
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name','last_name', 'email']
 
     def __repr__(self):
         return f"<User username={self.username}>"
@@ -48,7 +48,7 @@ class Answer(models.Model):
         User, on_delete=models.SET_NULL, related_name='answer', null=True)
     created_date = models.DateField(default=date.today)
     question = models.ForeignKey(
-        User, on_delete=models.SET_NULL, related_name='answers', null=True)
+        Question, on_delete=models.SET_NULL, related_name='answers', null=True)
     accepted = models.BooleanField(default=False)
     bookmark = models.ManyToManyField(User, related_name='bookmarked_answers', blank=True)
 
